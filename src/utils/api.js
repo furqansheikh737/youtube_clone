35c3a8ad28f984a6cfa3bccc58af751fba17f0cd
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2024-08-08 02:53:46 +0500

    header complete with search bar and icons

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com"
 const options = {
   params: { hl: 'en', gl: 'US' },
   headers: {
-    'x-rapidapi-key': process.env.REACT_APP_YOUTUBE_API_KEY,
+    'x-rapidapi-key': '8ef431d313mshc5b4f289d12c318p1378c4jsn7032b54bc101',
     'x-rapidapi-host': 'youtube138.p.rapidapi.com'
   },
 };
