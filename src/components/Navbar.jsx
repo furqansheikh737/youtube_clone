0b999ece70206773d003e358b83736a3e145cfc9
Furqan Sheikh <125441599+furqansheikh737@users.noreply.github.com>
2024-08-09 02:32:53 +0500

    create left nav menu done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,18 +74,18 @@ const Navbar = () => {
         </button>
       </div>
       <div className="flex items-center">
-                <div className="hidden md:flex">
-                    <div className="flex items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
-                        <RiVideoAddLine className="text-white text-xl cursor-pointer" />
-                    </div>
-                    <div className="flex items-center justify-center ml-2 h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
-                        <FiBell className="text-white text-xl cursor-pointer" />
-                    </div>
-                </div>
-                <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
-                    <img src="https://xsgames.co/randomusers/assets/avatars/female/67.jpg" />
-                </div>
-            </div>
+        <div className="hidden md:flex">
+          <div className="flex items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
+            <RiVideoAddLine className="text-white text-xl cursor-pointer" />
+          </div>
+          <div className="flex items-center justify-center ml-2 h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
+            <FiBell className="text-white text-xl cursor-pointer" />
+          </div>
+        </div>
+        <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
+          <img src="https://xsgames.co/randomusers/assets/avatars/female/67.jpg" />
+        </div>
+      </div>
     </div>
   )
 }
